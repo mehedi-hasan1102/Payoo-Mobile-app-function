ff7359bdbd885a47fcec7d89c5d154448f389f57
hasancodex <dev.hasan10@gmail.com>
2025-02-27 04:19:14 +0600

    log in complitte

--- script/login.js
+++ script/login.js
@@ -1,4 +1,24 @@
-document.getElementById('login-btn').addEventListener('click', function() {
-    console.log('login');
+document.getElementById('login-btn').addEventListener('click', function(event) {
+    event.preventDefault();
 
-})
+    const pin = document.getElementById('account-pin').value.trim();
+    const mobileNumber = document.getElementById('account-no').value.trim();
+
+    const convertedPin = parseInt(pin, 10);
+
+    if (!mobileNumber) {
+        console.log('Please enter your mobile number');
+        return;
+    }
+
+    if (isNaN(convertedPin) || pin.length !== 4) {
+        console.log('Invalid PIN. Please enter a 4-digit number');
+        return;
+    }
+
+    if (convertedPin === 1234) {
+        window.location.href = './main.html';
+    } else {
+        console.log('Login failed. Incorrect PIN');
+    }
+});
